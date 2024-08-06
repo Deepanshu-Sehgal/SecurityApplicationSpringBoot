b8470848d708458c7a9661418c7f8ba339d633a3
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-08-06 23:18:56 +0530

    init

--- src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/SecurityApplication.java
+++ src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/SecurityApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
-//592ef04c-e446-4544-8780-79caea52e8da
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 }
