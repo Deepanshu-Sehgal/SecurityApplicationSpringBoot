6f08e39e7e85daa1aa459ba0a1d51d16bf4d05c9
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-08-08 15:32:28 +0530

    init

--- src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/entities/User.java
+++ src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/entities/User.java
@@ -1,12 +1,16 @@
 package com.datricle.SpringSecurityDemo.SecurityApplication.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class User implements UserDetails {
 
