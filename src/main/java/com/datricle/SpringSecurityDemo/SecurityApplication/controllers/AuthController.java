201f22bee90bf67292ab8502a22e3cb657a114b8
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-08-09 12:19:03 +0530

    init

--- src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/controllers/AuthController.java
+++ src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/controllers/AuthController.java
@@ -6,9 +6,15 @@ import com.datricle.SpringSecurityDemo.SecurityApplication.dto.SignUpDTO;
 import com.datricle.SpringSecurityDemo.SecurityApplication.dto.UserDTO;
 import com.datricle.SpringSecurityDemo.SecurityApplication.services.AuthService;
 import com.datricle.SpringSecurityDemo.SecurityApplication.services.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,8 +31,11 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LogInDTO logInDTO){
+    public ResponseEntity<String> login(@RequestBody LogInDTO logInDTO, HttpServletRequest request, HttpServletResponse response) {
         String token = authService.logIn(logInDTO);
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
         return ResponseEntity.ok(token);
     }
 }
