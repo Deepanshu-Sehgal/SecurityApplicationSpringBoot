938b69ea46e7bc0eaa97e4101079306e943a31e4
Deepanshu-sehgal <deepanshusehgal.112@gmail.com>
2024-08-12 17:57:16 +0530

    init

--- src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/controllers/AuthController.java
+++ src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.datricle.SpringSecurityDemo.SecurityApplication.controllers;
 
 
 import com.datricle.SpringSecurityDemo.SecurityApplication.dto.LogInDTO;
+import com.datricle.SpringSecurityDemo.SecurityApplication.dto.LoginResponseDTO;
 import com.datricle.SpringSecurityDemo.SecurityApplication.dto.SignUpDTO;
 import com.datricle.SpringSecurityDemo.SecurityApplication.dto.UserDTO;
 import com.datricle.SpringSecurityDemo.SecurityApplication.services.AuthService;
@@ -11,10 +12,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,11 +35,23 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LogInDTO logInDTO, HttpServletRequest request, HttpServletResponse response) {
-        String token = authService.logIn(logInDTO);
-        Cookie cookie = new Cookie("token", token);
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LogInDTO logInDTO, HttpServletRequest request, HttpServletResponse response) {
+        LoginResponseDTO loginResponseDTO = authService.logIn(logInDTO);
+        Cookie cookie = new Cookie("refresh", loginResponseDTO.getRefreshToken());
         cookie.setHttpOnly(true);
         response.addCookie(cookie);
-        return ResponseEntity.ok(token);
+
+        return ResponseEntity.ok(loginResponseDTO);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDTO> refresh(HttpServletRequest request) {
+        String refreshToken = Arrays.stream(request.getCookies())
+                .filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .findFirst()
+                .map(cookie -> cookie.getValue())
+                .orElseThrow(() -> new AuthenticationServiceException("Refresh Token not found inside the cookies"));
+        LoginResponseDTO loginResponseDTO = authService.refreshToken(refreshToken);
+        return ResponseEntity.ok(loginResponseDTO);
     }
 }
--- src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/services/AuthService.java
+++ src/main/java/com/datricle/SpringSecurityDemo/SecurityApplication/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.datricle.SpringSecurityDemo.SecurityApplication.services;
 
 import com.datricle.SpringSecurityDemo.SecurityApplication.dto.LogInDTO;
+import com.datricle.SpringSecurityDemo.SecurityApplication.dto.LoginResponseDTO;
 import com.datricle.SpringSecurityDemo.SecurityApplication.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,13 +14,27 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserService userService;
 
-    public String logIn(LogInDTO logInDTO) {
+    public LoginResponseDTO logIn(LogInDTO logInDTO) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(logInDTO.getEmail(), logInDTO.getPassword())
         );
 
         User user = (User) authentication.getPrincipal();
-        return jwtService.generateToken(user);
+        String accessToken = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        return new LoginResponseDTO(user.getId(), accessToken, refreshToken);
+    }
+
+    public LoginResponseDTO refreshToken(String refreshToken) {
+        Long userId = jwtService.getUserFromToken(refreshToken);
+
+        User user = userService.getUserById(userId);
+
+        String accessToken = jwtService.generateAccessToken(user);
+        return new LoginResponseDTO(user.getId(), accessToken, refreshToken);
+
     }
 }
